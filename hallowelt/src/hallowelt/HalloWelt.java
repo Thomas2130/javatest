c560f68ca390971d96886cf12ffb2bea94f08faf
Thomas Demmer <thomasdemmer@gmx.net>
2016-12-30 17:41:48 +0100

    Test

--- hallowelt/src/hallowelt/HalloWelt.java
+++ hallowelt/src/hallowelt/HalloWelt.java
@@ -56,6 +56,8 @@ public class HalloWelt {
 		System.out.println("Test");
 		System.out.println("Test 2");
 		
+		System.out.println("Test");
+		
 		// other templates are placed under Window->References->Java->Editor->Templates
 
 		// strg + umschalt + t: search
